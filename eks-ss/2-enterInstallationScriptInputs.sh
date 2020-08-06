6c97c59aff7507a3c804b576566256dc98205aa9
rob jahn <robjahn@gmail.com>
2020-08-05 21:48:18 -0400

    remove dynatraceTenant

--- eks-ss/2-enterInstallationScriptInputs.sh
+++ eks-ss/2-enterInstallationScriptInputs.sh
@@ -14,7 +14,6 @@ then
     then 
       DEPLOYMENT=$1
     fi
-    DT_TENANT_ID=$(cat creds.json | jq -r '.dynatraceTenant')
     DT_HOSTNAME=$(cat creds.json | jq -r '.dynatraceHostName')
     DT_API_TOKEN=$(cat creds.json | jq -r '.dynatraceApiToken')
     DT_PAAS_TOKEN=$(cat creds.json | jq -r '.dynatracePaaSToken')
@@ -29,7 +28,6 @@ echo "==================================================================="
 echo "Please enter the values for provider: $DEPLOYMENT_NAME"
 echo "Press <enter> to keep the current value"
 echo "==================================================================="
-read -p "Dynatrace Tenant ID (e.g. abc12345)    (current: $DT_TENANT_ID) : " DT_TENANT_ID_NEW
 echo "Dynatrace Host Name"
 read -p "  (e.g. abc12345.live.dynatrace.com)   (current: $DT_HOSTNAME) : " DT_HOSTNAME_NEW
 read -p "Dynatrace API Token                    (current: $DT_API_TOKEN) : " DT_API_TOKEN_NEW
@@ -44,7 +42,6 @@ esac
 echo "==================================================================="
 echo ""
 # set value to new input or default to current value
-DT_TENANT_ID=${DT_TENANT_ID_NEW:-$DT_TENANT_ID}
 DT_HOSTNAME=${DT_HOSTNAME_NEW:-$DT_HOSTNAME}
 DT_API_TOKEN=${DT_API_TOKEN_NEW:-$DT_API_TOKEN}
 DT_PAAS_TOKEN=${DT_PAAS_TOKEN_NEW:-$DT_PAAS_TOKEN}
@@ -52,7 +49,6 @@ CLUSTER_REGION=${CLUSTER_REGION_NEW:-$CLUSTER_REGION}
 
 echo -e "Please confirm all are correct:"
 echo ""
-echo "Dynatrace Tenant             : $DT_TENANT_ID"
 echo "Dynatrace Host Name          : $DT_HOSTNAME"
 echo "Dynatrace API Token          : $DT_API_TOKEN"
 echo "Dynatrace PaaS Token         : $DT_PAAS_TOKEN"
@@ -76,7 +72,6 @@ then
 
     cat ./creds.sav | \
       sed 's~DEPLOYMENT_PLACEHOLDER~'"$DEPLOYMENT"'~' | \
-      sed 's~DYNATRACE_TENANT_PLACEHOLDER~'"$DT_TENANT_ID"'~' | \
       sed 's~DYNATRACE_HOSTNAME_PLACEHOLDER~'"$DT_HOSTNAME"'~' | \
       sed 's~DYNATRACE_API_TOKEN_PLACEHOLDER~'"$DT_API_TOKEN"'~' | \
       sed 's~DYNATRACE_PAAS_TOKEN_PLACEHOLDER~'"$DT_PAAS_TOKEN"'~' | \
